e08418bee60a76796b1ac42b74cfd5488827ee69
library-rat <1ucas_z@protonmail.com>
2024-01-22 22:00:49 +0100

    fixing offset on grid

--- Jeu souris/DangerOverlay.gd
+++ Jeu souris/DangerOverlay.gd
@@ -5,7 +5,7 @@ var board: Resource
 func _ready():
 	board =  load("res://Scène principale/Gameboard/Board.tres")
 	board.connect("update_threat", Callable(self, "_on_update_threat"))
-	draw(1, [Vector2i(0,0),Vector2i(1,0)], "rouge")
+	draw(0, [Vector2i(0,0)], "rouge")
 	
 
 func _on_update_threat():
